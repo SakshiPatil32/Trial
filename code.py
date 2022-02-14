893f34d5ec5da3dee39c37e696e580cb891a9e44
SakshiPatil32 <sakshi.p2023@gmail.com>
2022-02-14 12:30:29 +0530

    water

--- code.py
+++ code.py
@@ -3,4 +3,9 @@ h = input("enter y/n : ")
 if h == "y":
     print("samosa")
 else:
+    t = input("enter if thirsty y/n : ")
+    if t == 'y':
+        print("drink water")
+    else:
+        print("good")
     print("bye")
